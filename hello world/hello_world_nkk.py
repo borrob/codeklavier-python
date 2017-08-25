c87380d38a5bb6bba87ac860f7edc3693b90f959
Rob van Loon <borrob@me.com>
2017-08-25 23:12:40 +0200

    Move ugly class out of hell_world_nkk

--- hello world/hello_world_nkk.py
+++ hello world/hello_world_nkk.py
@@ -10,6 +10,7 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 from CodeKlavier.Setup import Setup
 from CodeKlavier.Mapping import Mapping_HelloWorld_NKK
+from CodeKlavier.Callback import Callback
 
 # Start the CodeKlavier
 codeK = Setup()
@@ -22,22 +23,13 @@ print('your device id is: ', device_id, '\n')
 mapping = Mapping_HelloWorld_NKK()
 
 # class to handle the midi input and map it to characters
-#TODO: this is ugly! Move this to the CodeKlavier module
-class HelloWorld(object):
-    def __init__(self, port):
-        self.port = port
-
-    def __call__(self, event, data=None):
-        message, deltatime = event
-        if message[2] > 0: #only noteOn
-            if (message[0] == device_id):
-                mapping.mapping(message[1])
+callback = Callback(device_id, myPort, mapping)
 
 print("\nCodeKlavier is ready and ON.")
 print("You are performing: HELLO WORLD")
 print("\nPress Control-C to exit.")
 
-codeK.set_callback(HelloWorld(myPort))
+codeK.set_callback(callback)
 
 # Loop to program to keep listening for midi input
 try:
