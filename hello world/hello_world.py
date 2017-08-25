6f37c043638224a25c1e8057134036bbc9158d81
Rob van Loon <borrob@me.com>
2017-08-25 23:22:44 +0200

    Merge hello_world and hello_world_nkk

--- hello world/hello_world.py
+++ hello world/hello_world.py
@@ -10,6 +10,7 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 from CodeKlavier.Setup import Setup
 from CodeKlavier.Mapping import Mapping_HelloWorld
+from CodeKlavier.Mapping import Mapping_HelloWorld_NKK
 from CodeKlavier.Callback import Callback
 
 # Start the CodeKlavier
@@ -20,7 +21,22 @@ device_id = codeK.get_device_id()
 print('your device id is: ', device_id, '\n')
 
 # Use your favourite mapping of the keys
-mapping = Mapping_HelloWorld()
+print("Which keyboard map do you want to use?")
+print("  1. Hello World (normal)")
+print("  2. Hello World (NKK)")
+choice_ok = False
+while not(choice_ok):
+    map_number = input("__?")
+    if(int(map_number) == 1):
+        mapping = Mapping_HelloWorld()
+        choice_ok = True
+        break
+    elif(int(map_number) ==2 ):
+        mapping = Mapping_HelloWorld_NKK()
+        choice_ok = True
+        break
+    else:
+        print("Please type 1 or 2.")
 
 # class to handle the midi input and map it to characters
 callback = Callback(device_id, myPort, mapping)
